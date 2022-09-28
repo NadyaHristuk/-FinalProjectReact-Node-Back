beec7f4dbe39373fa95f1cb31cab49e0d3390cf0
nkhrystiuk <elhe2013@gmail.com>
2022-09-28 13:07:43 +0300

    q

--- app.js
+++ app.js
@@ -24,7 +24,8 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api-docs', (_, res, next) => {res.send("sss")});
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 // app.use(express.static("public"))
 
 // app.use('/api/auth', authRouter);
